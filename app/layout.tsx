7b5c4ff1975fc08e070e6141b20ce7c74eeea49a
sahana <ssmoogi@gmail.com>
2024-06-11 15:17:20 -0700

    progress day 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased bg-lavender`}>
           <div className="min-h-screen min-w-screen place-content-center">
-            <div className={`${inter.className} flex-grow w-3/5 h-9/12 m-auto p-12 overflow-y-auto border-black border-2 bg-white rounded-lg`}>{children}</div>
+            <div className={`${inter.className} flex-grow w-3/5 h-9/12 m-auto p-12 overflow-y-auto border-black border-[3px] bg-white rounded-2xl`}>{children}</div>
           </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,26 @@ export default function Home() {
     <main className="flex min-h-full flex-col items-right justify-between p-[16px]">
         <div className="min-h-full">
             <div className="min-h-min">
-                <div>
-                    <p className="text-black text-7xl p-3"><strong>sahana moogi.</strong></p>
+                <div className="flex flex-row">
+                    <p className="text-black text-7xl p-3 pb-5 hover:bg-dark hover:text-lavender transition-colors"><strong>sahana moogi.</strong></p>
                 </div>
+                {/*<div className="flex flex-row">
+                    <Image
+                        src="/sahana.jpeg"
+                        width={150}
+                        height={150}
+                        className="block border-black border-2 rounded-lg m-3"
+                        alt="picture of me"
+                    />
+                </div> */}
                 <p className="text-black text-lg p-3">
                     Hello there! My name is Sahana. I'm from the Bay Area, currently located in Berkeley, California.
                     I'm currently a student pursuing a simultaneous degree in Electrical Engineering & Computer Science (EECS) and Music, which consumes most of my time so I felt like I should mention.
                 </p>
                 <p className="text-black text-lg p-3">
-                    Defining ourselves is more complex than we realize! But to summarize, I love music, anything <Link href="/" className="text-purple underline underline-offset-2 hover:bg-purple hover:text-white hover:no-underline transition-colors">caffeinated</Link>, building <Link href="/projects" className="text-purple underline underline-offset-2 hover:bg-purple hover:text-white hover:no-underline transition-colors">things</Link>, biking, conversations, and spending time with people." 
+                    Defining ourselves is more complex than we realize! At least, I have realized, because of how much of our personality is context-driven. And how hard it is to put a consistent definition to who we are all the time. We're constantly changing. But to summarize, I love music, anything <Link href="/" className="text-purple underline underline-offset-2 hover:bg-purple hover:text-white hover:no-underline transition-colors">caffeinated</Link>, building <Link href="/projects" className="text-purple underline underline-offset-2 hover:bg-purple hover:text-white hover:no-underline transition-colors">things</Link>, biking, conversations, and spending time with people. Usually down for anything, email me at ssmoogi at gmail dot com! <Link href="/world" className="text-white bg-purple hover:bg-lavender pl-2 pr-2 hover:text-dark hover:no-underline transition-colors">enter ↵</Link>
                 </p>
-                <div className="p-3 text-xl text-dark">
+                <div className="p-3 pt-6 text-xl text-dark">
                     {[
                         ['~github~', 'https://github.com/ssmoogi'],
                         ['~spotify~', 'https://open.spotify.com/user/ssmoogi'],
